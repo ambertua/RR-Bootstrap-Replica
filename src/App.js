cbaccc1bf36d5c0b5d6ae5a98b349f39715cbef3
ambertua <amber.tuamoheloa@yahoo.com>
2023-05-24 21:30:48 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import splashImage from './embedded/background.webp';
+import TopBar from './components/TopBar';
+import styled from 'styled-components';
+import StoreListing from './components/StoreListing';
+
+const Splash = styled.div`
+  width: 100vw;
+  height: 250px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${ splashImage });
+  background-color: #dfeed6;
+  position: absolute;
+  left: 0;
+  top:40px;
+  right: 0;
+  z-index: -1
+`
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <>
+    <TopBar />
+    <Splash />
+    <StoreListing />
+  </>
 }
 
 export default App;
